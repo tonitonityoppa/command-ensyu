6139e3fedaa5e65cc51d72c917bfc88318ecd1cf
sutotakato <t.suto.sys22@morijyobi.ac.jp>
2022-11-08 12:56:11 +0900

    add .gitignore

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("・身長(m)を入力してください：");
+    double height = sc.nextDouble();
+    System.out.print("・体重(Kg)を入力してください：");
+    double weight = sc.nextDouble();
+    double bmi = weight / (height*height);
+    String sv = String.format("%.2f",bmi);
+    double bmi2 = Double.parseDouble(sv);
+    System.out.println("BMI値は"+bmi2+"です。");
+  }
+}
