794f91be238765beaf50c0e7d566100108900ed6
sutotakato <t.suto.sys22@morijyobi.ac.jp>
2022-11-08 13:26:07 +0900

    二人分の計算

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+
+    for (int i=0; i < 2; i++){
     System.out.print("・身長(m)を入力してください：");
     double height = sc.nextDouble();
     System.out.print("・体重(Kg)を入力してください：");
@@ -10,5 +12,6 @@ public class Main {
     String sv = String.format("%.2f",bmi);
     double bmi2 = Double.parseDouble(sv);
     System.out.println("BMI値は"+bmi2+"です。");
+    }
   }
 }
